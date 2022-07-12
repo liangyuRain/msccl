910786ca18355847b5ec5a6f10d7f7d1946cd68b
Liangyu <liangyu@cs.washington.edu>
2022-07-11 18:26:09 -0700

    tunning fix for RS and AG

--- src/graph/tuning.cc
+++ src/graph/tuning.cc
@@ -294,13 +294,14 @@ static float treeCorrectionFactor[NCCL_NUM_PROTOCOLS][23] = {
 
 ncclResult_t ncclTopoGetAlgoTime(struct ncclInfo* info, int algorithm, int protocol, float* time, int* scclAlgoIndex, struct ncclComm* comm) {
   if (algorithm == NCCL_ALGO_SCCL){
+    size_t countMultiplied = info->nBytes/ncclTypeSize(info->datatype);
     if (comm->nScclRegistrations > 0) {
       for (int i = 0; i < comm->nScclRegistrations; ++i) {
         struct scclRegistration *reg = &comm->scclRegistrations[i];
         if (reg->minBytes <= info->nBytes && (info->nBytes < reg->maxBytes || reg->maxBytes == -1) && reg->protocol == protocol) {
           struct scclAlgorithm* scclAlgo = &comm->scclAlgos[reg->algoIndex];
           if ((scclAlgo->isValid) && (scclAlgo->collectiveType == info->coll) && (info->inplace == scclAlgo->inPlace) && (scclAlgo->ngpus == info->comm->nRanks)
-              && ((info->count % scclAlgo->nchunksPerLoop) == 0)) {
+              && ((countMultiplied % scclAlgo->nchunksPerLoop) == 0)) {
             *time = 0.f;
             *scclAlgoIndex = reg->algoIndex;
             return ncclSuccess;
@@ -311,7 +312,7 @@ ncclResult_t ncclTopoGetAlgoTime(struct ncclInfo* info, int algorithm, int proto
       for (int i=0; i<comm->numberOfSCCAlgorithms; i++){
         struct scclAlgorithm* scclAlgo = &comm->scclAlgos[i];
         if ((scclAlgo->isValid) && (scclAlgo->collectiveType == info->coll) && (info->inplace == scclAlgo->inPlace) && (scclAlgo->protocol == protocol) && (scclAlgo->ngpus == info->comm->nRanks)
-            && ((info->count % scclAlgo->nchunksPerLoop) == 0) && (info->nBytes >= scclAlgo->minBytes) && (info->nBytes < scclAlgo->maxBytes)) {
+            && ((countMultiplied % scclAlgo->nchunksPerLoop) == 0) && (info->nBytes >= scclAlgo->minBytes) && (info->nBytes < scclAlgo->maxBytes)) {
           *time = 0.f;
           *scclAlgoIndex = i;
           return ncclSuccess;
